ffd48d2b5595489c3d34aea8e457830a3d5ccb03
Iambankjaa <ikakzudzud1@hotmail.com>
2018-05-01 09:39:43 +0700

    have slider and bug filter

--- Gui.py
+++ Gui.py
@@ -5,6 +5,13 @@ import hashlib
 from  tkinter import filedialog
 from tkinter import *
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.pyplot import Figure
+from matplotlib.widgets import Slider
+# from test import Checkbar
 
 
 class Gui:
@@ -17,6 +24,9 @@ class Gui:
         self.Agg_Column2 = list()
         self.Agg_Row = dict()
         self.Agg_Row2 = list()
+        self.Agg_Filter = list()
+        self.list_column = list()
+        self.listfilter = list()
 
         import Data
         self.data = Data.Data()
@@ -24,6 +34,8 @@ class Gui:
         self.ListBoxDimensions()
         self.ListBoxMeasures()
         self.ListBoxColumnsRows()
+        self.ListBoxFilter()
+        self.ListBoxWFilter()
         self.Label()
 
     def Label(self):
@@ -32,39 +44,76 @@ class Gui:
         self.lable1.grid(column=0, row=1)
         self.lable2 = Label(self.root, text = "Measures")
         self.lable2.grid(column=0, row=3)
-        self.lable2 = Label(self.root, text = "Column :")
-        self.lable2.grid(column=1, row=0)
-        self.lable2 = Label(self.root, text = "Row :")
-        self.lable2.grid(column=3, row=0)
+        self.lable3 = Label(self.root, text = "Column :")
+        self.lable3.grid(column=1, row=0)
+        self.lable4 = Label(self.root, text = "Row :")
+        self.lable4.grid(column=3, row=0)
+        self.lable5 = Label(self.root, text = "Select to Filter :")
+        self.lable5.grid(column=5, row=0)
+        self.lable6 = Label(self.root, text = "Filter :")
+        self.lable6.grid(column=7, row=0)
+        self.lable7 = Label(self.root, text = "Plot :")
+        self.lable7.grid(column=1, row=2)
 
     def Button(self):
         # print("class Gui Button")
 
         self.root.title('App')
-        self.root.geometry("1200x720")
+        self.root.geometry("900x720")
 
         Browse_Button= tk.Button(self.root, text="Browse", command = self.BrowseCommand)
         Browse_Button.grid(column=0, row=0)
 
-        Add_Columns_Button= tk.Button(self.root, text="Add to Column", command = self.AddColumnsCommand)
-        Add_Columns_Button.grid(column=2, row=1)
+        Add_Columns_Button= tk.Button(self.root, text="Add", command = self.AddColumnsCommand)
+        Add_Columns_Button.grid(column=2, row=1,sticky = "w")
 
-        Add_Rows_Button= tk.Button(self.root, text="Add to Row", command = self.AddRowsCommand)
-        Add_Rows_Button.grid(column=4, row=1)
+        Add_Rows_Button= tk.Button(self.root, text="Add", command = self.AddRowsCommand)
+        Add_Rows_Button.grid(column=4, row=1,sticky = "w")
 
-        Clear_Columns_Button= tk.Button(self.root, text="Clear Dimensions", command = self.ClearColumnsCommand)
-        Clear_Columns_Button.grid(column=2, row=2)
+        Clear_Columns_Button= tk.Button(self.root, text="Delete", command = self.ClearColumnsCommand)
+        Clear_Columns_Button.grid(column=2, row=1,sticky = "e")
 
-        Clear_Rows_Button= tk.Button(self.root, text="Clear Measures", command = self.ClearRowsCommand)
-        Clear_Rows_Button.grid(column=4, row=2)
+        Clear_Rows_Button= tk.Button(self.root, text="Delete", command = self.ClearRowsCommand)
+        Clear_Rows_Button.grid(column=4, row=1,sticky = "e")
 
         Change_Button= tk.Button(self.root, text="Change", command = self.ChangeCommand)
         Change_Button.grid(column=0, row=5)
 
+        Add_Filter = tk.Button(self.root, text="Add to Filter", command = self.AddFilterCommand)
+        Add_Filter.grid(column=6, row=1)
+
+        Clear_Filter = tk.Button(self.root, text="Delete to Filter", command = self.ClearFilterCommand)
+        Clear_Filter.grid(column=8, row=1)
+
+        Select_Filter = tk.Button(self.root, text="Filter", command = self.Add_Filter)
+        Select_Filter.grid(column=2, row=1,sticky = "n")
+
         Draw_Button = tk.Button(self.root, text="Draw", command = self.showchart)
-        Draw_Button.grid(column=5, row=0)
+        Draw_Button.grid(column=9, row=0)
+
+        # lng = Checkbar(self.root, ['Python', 'Ruby', 'Perl', 'C++'])
+        # lng.grid(column=7, row=0)
+
+        # Clear_Button = tk.Button(self.root, text="Clear", command = self.clear_data)
+        # Clear_Button.grid(column=6, row=0)
 
         self.root.mainloop()
+    def Add_Filter(self):
+        self.ListBox_WaitFilter.delete(0,END)
+        #import collections
+        uni = self.file[self.valueCol].unique()
+        self.colforfil = self.valueCol#name column for filter
+        for i in uni:
+            self.ListBox_WaitFilter.insert(END,i)
+
+        # lng = Checkbar(top, self.list)
+        # lng.grid(row=0, column=0)
+
+    def Filter(self):
+        print(self.listfilter)
+    #     # for i in self.ListBox_Filter
+    #     #     print(i)
+
 
     def BrowseCommand(self):
         # print("class Gui BrowseCommand")
@@ -82,25 +131,7 @@ class Gui:
             #self.data.CheckType(self.file,List_Header)
             self.data.CheckMd5(self.List_Header, md5, self.file)
 
-            #self.data.CheckDB(List_Header)
-            #print(self.data.Data_Dimensions[0])
-            #print(self.file.groupby(List_Header[0]).count())
-            #print(pd.groupby([List_Header[0],List_Header[1]])[List_Header[6]].agg('sum'))
-            #print(self.file.groupby([List_Header[0],List_Header[1],List_Header[5],List_Header[8]])[List_Header[6],List_Header[13]].agg('sum'))#[List_Header[6]].agg('sum'))
 
-#AGG = {List_Header[6]:'count',List_Header[13]:'sum'}
-            #AGG['{}'.format(List_Header[14])] = 'count'
-#AGG['{}'.format(List_Header[14])] = '{}'.format("count")
-            #AGG[List_Header[14]] = 'count'
-#print(self.file.groupby([List_Header[0],List_Header[1],List_Header[5],List_Header[8]]).agg(AGG))#[List_Header[6]].agg('sum'))
-            #df.groupby(['col1', 'col2']).agg({'col3':'sum','col4':'sum'})
-            #print(pd.groupby([self.data.Data_Dimensions[0],self.data.Data_Dimensions[1]]).sum())
-            #grouper = pd.groupby(["{}".format(List_Header[0])])
-            #s = grouper.count()
-            #print(s)
-
-            # print(self.data.Data_Dimensions)
-            # print(self.data.Data_Measures)
             self.List_Dimensions = list()
             try:
                 self.List_Dimensions = self.data.Data_Dimensions
@@ -116,13 +147,26 @@ class Gui:
         except FileNotFoundError:
             pass
 
+    def AddFilterCommand(self):
+        # print("class Gui AddDimensionsCommand")
+        try:
+            print(self.valuewaitFil)#value for filter
+            self.ListBox_Filter.insert(END,self.valuewaitFil)
+            #if len(self.listfilter) > 0:
+                #for i in range(len(self.listfilter)):
+                    #if self.listfilter[i][0] == self.valuewaitFil:
+            #else:
+            self.listfilter.append([self.colforfil,self.valuewaitFil])
+            self.Filter()
+        except AttributeError:
+            pass
+
     def AddColumnsCommand(self):
         # print("class Gui AddDimensionsCommand")
         try:
             self.ListBox_Columns.insert(END,self.value)
             self.AggDataColumn()
-            self.data.DrawChart(self.Agg_Column, self.Agg_Column2,
-                                self.Agg_Row, self.Agg_Row2)
+
         except AttributeError:
             pass
 
@@ -131,9 +175,24 @@ class Gui:
        try:
             self.ListBox_Rows.insert(END,self.value)
             self.AggDataRow()
-            self.data.DrawChart(self.Agg_Column, self.Agg_Column2,
-                                self.Agg_Row, self.Agg_Row2)
+
        except AttributeError:
+            pass
+
+    def ClearFilterCommand(self):
+        # print("class Gui ClearColumnsCommand")
+        try:
+            # print(type(str(self.ListBox_Filter.curselection())))
+            # print(self.listfilter)
+            # print(str(self.ListBox_Filter.curselection()))
+            # print((self.ListBox_Filter.curselection()))
+            # A = self.ListBox_Filter.get(self.ListBox_Filter.curselection())
+            for i in self.listfilter:
+                if i[1] == self.ListBox_Filter.get(self.ListBox_Filter.curselection()):
+                    self.listfilter.remove(i)
+            self.ListBox_Filter.delete(self.ListBox_Filter.curselection())
+            self.Filter()
+        except TclError:
             pass
 
     def ClearColumnsCommand(self):
@@ -141,8 +200,6 @@ class Gui:
         try:
             self.ListBox_Columns.delete(self.ListBox_Columns.curselection())
             self.AggDataColumn()
-            self.data.DrawChart(self.Agg_Column, self.Agg_Column2,
-                                self.Agg_Row, self.Agg_Row2)
         except TclError:
             pass
 
@@ -152,8 +209,6 @@ class Gui:
         try:
             self.ListBox_Rows.delete(self.ListBox_Rows.curselection())
             self.AggDataRow()
-            self.data.DrawChart(self.Agg_Column, self.Agg_Column2,
-                                self.Agg_Row, self.Agg_Row2)
         except TclError:
             pass
 
@@ -227,6 +282,17 @@ class Gui:
         self.ListBox_Rows.grid(column=4, row=0)
         self.ListBox_Rows.bind('<<ListboxSelect>>', self.CurSelectRows)
 
+    def ListBoxWFilter(self):
+        self.ListBox_WaitFilter = Listbox(self.root)
+        self.ListBox_WaitFilter.grid(column=6, row=0)
+        self.ListBox_WaitFilter.bind('<<ListboxSelect>>', self.CurSelectWFilter)
+
+    def ListBoxFilter(self):
+        self.ListBox_Filter = Listbox(self.root)
+        self.ListBox_Filter.grid(column=8, row=0)
+        self.ListBox_Filter.bind('<<ListboxSelect>>', self.CurSelectFilter)
+
+
 
 
     def CurSelectDimensions(self, evt):
@@ -235,7 +301,6 @@ class Gui:
             print(self.value)
         except:
             pass
-
 
     def CurSelectMeasures(self, evt):
         try:
@@ -246,7 +311,8 @@ class Gui:
 
     def CurSelectColumns(self,evt):
         try:
-            self.value = None
+            self.valueCol = str(self.ListBox_Columns.get(self.ListBox_Columns.curselection()))
+            print(self.valueCol)
         except:
             pass
 
@@ -254,6 +320,20 @@ class Gui:
         try:
             self.value = None
 
+        except:
+            pass
+
+    def CurSelectFilter(self,evt):
+        try:
+            self.valueFil = str(self.ListBox_Filter.get(self.ListBox_Filter.curselection()))
+            print(self.valueFil)
+        except:
+            pass
+
+    def CurSelectWFilter(self,evt):
+        try:
+            self.valuewaitFil = str(self.ListBox_WaitFilter.get(self.ListBox_WaitFilter.curselection()))
+            print(self.valuewaitFil)
         except:
             pass
 
@@ -304,89 +384,107 @@ class Gui:
         print(self.Agg_Row2)
 
     def showchart(self):
+
         print(self.Agg_Column)
         print(self.Agg_Column2)
         print(self.Agg_Row)
         print(self.Agg_Row2)
-        # try:
-        AGG = {}
+        list1 = list()
+
+        try:
+            list1.append([self.listfilter[0][0],[self.listfilter[0][1]]])
+        except:
+            pass
+        for i in range(len(self.listfilter)):
+            for j in range(len(list1)):
+                if (self.listfilter[i][0] == list1[j][0] and i != 0):
+                    list1[j][1].append(self.listfilter[i][1])
+                    break
+                elif j == len(list1) - 1 and i != 0:
+                    list1.append([self.listfilter[i][0],[self.listfilter[i][1]]])
+
+        for i in range(len(list1)):
+                print(list1[i][0])
+                print(list1[i][1])
+                self.df_filtered = self.file.loc[self.file[list1[i][0]].isin(list1[i][1])]
+
+        print("44444")
+        print(len(self.listfilter))
+        print(list1)
+        print(len(self.listfilter))
+        print("545555")
+        self.AGG = {}
+
         for key in self.Agg_Row:
-            AGG[key] = self.Agg_Row[key]
-            print(AGG)
+            self.AGG[key] = self.Agg_Row[key]
+            print(self.AGG)
+
         for key1 in self.Agg_Column:
-            AGG[key1] = self.Agg_Column[key1]
-        # except:
-        #     pass
-        # print(self.file.groupby([self.List_Header[0]]).agg(AGG))
-        column = 10
-        # # for j in self.Agg_Column2:
-        # #     print(j)
-        # #     print(self.Agg_Column2[0])
-        # #     print(self.Agg_Column2[1])
-        # #     print(AGG)
-        d = self.file.groupby(self.Agg_Column2).agg(AGG)
-        # d1 = self.file.groupby(self.Agg_Column2[0]).agg(d)
-        print(d)
-        # print(self.Agg_Column1)
-        # print(self.Agg_Row2)
-        import matplotlib
-        matplotlib.use('TkAgg')
+            self.AGG[key1] = self.Agg_Column[key1]
 
-        from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-        from matplotlib.pyplot import Figure
-
+        column = 2
         top = tk.Frame(self.root)
-        top.grid(column=column, row=0 ,columnspan = 10 ,rowspan = 10)
+        top.grid(column=column, row=2 ,columnspan = 10 ,rowspan = 10)
 
-        fig = matplotlib.pyplot.Figure()
+        self.fig = matplotlib.pyplot.Figure()
 
-        canvas = FigureCanvasTkAgg(fig, top)
+        canvas = FigureCanvasTkAgg(self.fig, top)
 
         canvas.get_tk_widget().pack()
 
         toolbar = NavigationToolbar2TkAgg(canvas, top)
         toolbar.update()
         canvas._tkcanvas.pack()
+        self.ax1 = self.fig.add_subplot(111)
 
-        ax1 = fig.add_subplot(111)
-
-        # draw on this plot
-        d.plot(kind='bar', legend=False, ax=ax1)
-        # column = column+10
-        for l in self.Agg_Row2:
-            print(l)
-            print(AGG)
-            d = self.file.groupby([l]).agg(AGG)
-            # print(self.Agg_Column1)
-            # print(self.Agg_Row2)
-            import matplotlib
-            matplotlib.use('TkAgg')
-
-            from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-            from matplotlib.pyplot import Figure
-
-            top = tk.Frame(self.root)
-            top.grid(column=10, row=0 ,columnspan = 10 ,rowspan = 10)
-
-            fig = matplotlib.pyplot.Figure()
-
-            canvas = FigureCanvasTkAgg(fig, top)
-
-            canvas.get_tk_widget().pack()
-
-            toolbar = NavigationToolbar2TkAgg(canvas, top)
-            toolbar.update()
-            canvas._tkcanvas.pack()
-
-            ax1 = fig.add_subplot(111)
-
-            # draw on this plot
-            d.plot(kind='bar', legend=True, ax=ax1)
-            row = row+10
+        # try:
+        self.d = self.file.groupby(self.Agg_Column2).agg(self.AGG)
+        print(self.d)
+        if self.listfilter == []:
+            self.d.plot(kind='bar', legend=True, ax=self.ax1)
+            maxslide = len(self.d.count(1))
+        else:
+            print("babo")
+                #print(self.file.loc[self.file[list1[0][0]].isin(list1[0][1])])
+            self.df_filtered.plot(kind='bar', legend=True, ax=self.ax1)
+            maxslide = len(self.df_filtered.count(1))
+        # except:
+        #     print('error')
+        #     print(self.AGG)
+            # self.d = self.file.groupby(self.Agg_Row2).agg(self.AGG)
+            # if self.listfilter == []:
+            #     self.d.plot(kind='bar', legend=True, ax=self.ax1)
+            #     maxslide = len(self.d.count(1))
+            # else:
+            #     self.df_filtered = self.d.filter(like=self.listfilter[0], axis=0)
+            #     # draw on this plot
+            #     print(self.df_filtered)
+            #     self.df_filtered.plot(kind='bar', legend=True, ax=self.ax1)
+            #     maxslide = len(self.df_filtered.count(1))
+        if maxslide > 10:
+            ax2 = self.fig.add_subplot(911)
+            self.slide = Slider(ax2, "X Axis", 0.0, maxslide-10,valinit=0)
+            self.ax1.set_xlim([0,9])
+            self.slide.on_changed(self.update)
         self.root.mainloop()
 
-        #
-        # pass
+    def update(self,val):
+    #     self.postion = self.slide.val
+        self.postion = self.slide.val
+        print(self.postion)
+        self.ax1.set_xlim([self.postion,self.postion+10])
+        self.fig.canvas.draw_idle()
+
+
+
+
+
+
+
 
 gui = Gui()
 gui.Button()
+
+
+
+
